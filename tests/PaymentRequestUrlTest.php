5b5908949b4a93fc4c9bcc61f686316f35d492f7
adam <adam.stipak@gmail.com>
2022-09-24 20:34:47 +0200

    Fix tests.

--- tests/PaymentRequestUrlTest.php
+++ tests/PaymentRequestUrlTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class PaymentRequestUrlTest extends TestCase {
 
   public function testCreateUrl () {
-    $expectedUrl = "http://test.webpay.com/foo/csob.do?MERCHANTNUMBER=1234&OPERATION=CREATE_ORDER&ORDERNUMBER=1234&AMOUNT=980&CURRENCY=978&DEPOSITFLAG=0&URL=http%3A%2F%2Ffoo.bar&ADDINFO=%3C%3Fxml+version%3D%221.0%22%3F%3E%0A%3CadditionalInfoRequest+xmlns%3D%22http%3A%2F%2Fgpe.cz%2Fgpwebpay%2FadditionalInfo%2Frequest%22+version%3D%224.0%22%2F%3E&DIGEST=uw238QYP2BscxGGOh4thMN58tYhlKthqkMb2rd%2BavV2xWwvrHe82kbt7xyWGFyanj5rdYhHMLZsiwVC9gHzK%2Ftg%2B0cgzbmdMaQyLmaOVwPKAoCVlrPp40MLDAFNRtwJF3Ihg%2BMNJWYhyd%2Ffx0QK1R%2BL1DlqpuiAFtn7AfzuzzLS2Y4cx8jfzm%2FnF5ZeTqfZaWPxXqoPlAs39Ph0YHCnieKlByJznvwsSrSS5L6HJEqL%2FMS%2Bg9dIDv%2FkfaszheOWEx%2Bwh2ntgiOc9W8f%2BENrI2JDaA1hvO9F7dSEeKoRf%2BLWFQUY%2BictN9W4b2SZozqttf1ovAoYNwE48qJ2bDnHNzw%3D%3D";
+    $expectedUrl = "http://test.webpay.com/foo/csob.do?MERCHANTNUMBER=1234&OPERATION=CREATE_ORDER&ORDERNUMBER=1234&AMOUNT=980&CURRENCY=978&DEPOSITFLAG=0&URL=http%3A%2F%2Ffoo.bar&PAYMETHOD=CRD&ADDINFO=%3C%3Fxml+version%3D%221.0%22%3F%3E%0A%3CadditionalInfoRequest+xmlns%3D%22http%3A%2F%2Fgpe.cz%2Fgpwebpay%2FadditionalInfo%2Frequest%22+version%3D%224.0%22%2F%3E&DIGEST=Fb0QQzkPv%2B9MAyvAhVrBjL4LjFv8oq4sQO7iSFZ7ZOgC9loqNgTLfPotUjRxcWFn3y0Dz60VAqcanmL%2FcGp2cAg%2FCqBb1OrvgpWeFECJ1tfyJ9ew7Nqhk4glu8Bs%2FiqH21xHQ0B7k1iglBIL00Xpd%2BOYnv37WHaZRm5i8rTXpGntvdWQx1Rb2SjT47Av1%2BeFz8UGtxorghCM2A0HPyc%2BcJ7qoHZJYrjFiaENBzYTQEh%2BcZaKP%2FxZMGVtEV6HGqgekJfI6oOXBHZJYJ8uMqvBE4OkBgfonbp728EopQSKOLHz7NRj7wNFM%2FNiFzTeaoof3KYq7G5PbcfImDR4LGpzbQ%3D%3D";
 
     $request = new PaymentRequest(
       1234,
@@ -30,7 +30,7 @@ class PaymentRequestUrlTest extends TestCase {
   }
 
   public function testCreateUrlWitNotAddInfo () {
-    $expectedUrl = "http://test.webpay.com/foo/csob.do?MERCHANTNUMBER=1234&OPERATION=CREATE_ORDER&ORDERNUMBER=1234&AMOUNT=980&CURRENCY=978&DEPOSITFLAG=0&URL=http%3A%2F%2Ffoo.bar&DIGEST=Y%2BEFNoZgv0N%2B06PkOXe7v6lg4jilLcYKhWA9NDZgh2Y51vRf0Tt5N8KbPqHsWaNXUoDZ598OJgqC5NeG7km%2FiNh29uyQvYQuXaFEjA77QVWUZz6MgrI2VZU7XObyhC%2FETJ5UruAcxgpUAwCnAnWSz374%2BPzkfuS1OHxQEK4UFEam3kns06fbyR2mloa4a6xduiRt9j%2Buy6YXGoe%2FycxrOUfUPug79XZRjF7gmUgAnIvCIUcqD%2BT2mlUmG7BtzuD2pCTyV3RV47lHhO5gLGBN1VFBDm%2BNO6zqM4WTkz9ZtJmsjbzTWX3MEmQgHiiJ9mDd%2FgWY1ipWFWz%2F7TQeZEwctg%3D%3D";
+    $expectedUrl = "http://test.webpay.com/foo/csob.do?MERCHANTNUMBER=1234&OPERATION=CREATE_ORDER&ORDERNUMBER=1234&AMOUNT=980&CURRENCY=978&DEPOSITFLAG=0&URL=http%3A%2F%2Ffoo.bar&PAYMETHOD=CRD&DIGEST=AhzYS0oZggIdpDZwPMjg9pIHciCFUMSn07tU8VtIl1WDK0WNJBsJkh8mfmlQgKcBHACI04ZfNUlSt7p2dzAj7f6YqFpxPPGwH2STogh7w5pNbUqYoxN6BlrmGdZZ72%2FUciXbMBsC2fkyc7KErixoJDT1QIFn46OVYjdy%2FQi4jAfLtT%2BTCXuo3QG5h8utcsRxeuyC5CSG44x%2Fz32ttMxaxWbTn0nBLtkncPmp54yc4vRBB6qM1qoyNkRBMl9mzQbzYaQfv%2BWsBM0Cg1d5DfM75jfFN24eBLkW3qvMjgKg%2BBWfcEPcQBnV3ZwOU%2BUaZz9etXf7dBgng3Osb56ErpQ25w%3D%3D";
 
     $request = new PaymentRequest(
       1234,
